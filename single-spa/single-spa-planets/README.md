0f935af0be37c289a671b0ced36c1cf714d78aca
Kacper Krzywiec <krzywiec@gmail.com>
2021-02-01 08:42:49 +0100

    single-spa-planets

---
+++ single-spa/single-spa-planets/README.md
@@ -0,0 +1,15 @@
+```
+cd root-config
+pnpm install
+pnpm start
+```
+
+open http://localhost:8080
+
+```
+cd planets
+pnpm install
+pnpm start -- --https --port 9001
+```
+
+open http://localhost:9001
---
+++ single-spa/single-spa-planets/planets/webpack.config.js
@@ -0,0 +1,15 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "react-mf",
+    projectName: "planets",
+    webpackConfigEnv,
+  });
+
+  return merge(defaultConfig, {
+    // customizations go here
+    externals: [/^rxjs\/?.*$/],
+  });
+};
